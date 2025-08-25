fe384653dc4df6e080188050026ee80b47982edd
Clickadelic <toby.hopp@gmail.com>
2025-08-25 12:03:26 +0200

    Footer ausgelagert

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -21,21 +21,12 @@
     </head>
     <body class="min-h-screen flex flex-col bg-slate-100">
         @include('components.header')
-        <div class="w-full h-[400px] bg-sky-800 flex items-center justify-center bg-camera">
+        <div class="w-full py-12 bg-sky-800 flex items-center justify-center bg-camera">
             <h2 class="text-white text-4xl">GFN Movie DB</h2>
         </div>
         <main class="container mx-auto grow">
             @yield('content')
         </main>
-        <footer class="bg-gray-900 text-white py-8">
-            <div class="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-sm">
-            <p>&copy; 2025 Meine Extension</p>
-            <div class="space-x-4 mt-4 md:mt-0">
-                <a href="/datenschutz" class="hover:underline">Datenschutz</a>
-                <a href="/impressum" class="hover:underline">Impressum</a>
-                <a href="mailto:support@example.com" class="hover:underline">Support</a>
-            </div>
-            </div>
-        </footer>
+        @include('components.footer');
     </body>
 </html>
